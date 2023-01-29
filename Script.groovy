284a16d48b8c8149676e871b5813cd7ccea8dff3
muhammedelgendy97 <61365303+muhammedelgendy97@users.noreply.github.com>
2023-01-29 22:18:37 +0200

    Create Script.groovy

---
+++ Script.groovy
@@ -0,0 +1,16 @@
+def buildApp(){
+          echo "Building the application ${params.VERSION}"
+          }
+          
+def testApp(){
+echo "Building the application ${params.VERSION}"
+}
+
+def deployApp(){
+echo "Building the application ${params.VERSION}"
+}
+
+return this 
+
+
+                     
